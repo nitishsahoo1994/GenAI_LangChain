510373f2b9b0201bb7ab37de1ac9ed640f103d58
SG0705787 <nsahoo1994@yahoo.com>
2026-05-11 12:23:32 +0530

    modified txt file

--- 8.RAG_docLoader/1.textLoader.py
+++ 8.RAG_docLoader/1.textLoader.py
@@ -5,6 +5,8 @@ from langchain_community.document_loaders import TextLoader
 loader =TextLoader('cricket.txt',encoding='utf-8')
 docs = loader.load()
 
+
+p
 print(docs[0].page_content)
 
 print(len(docs))
